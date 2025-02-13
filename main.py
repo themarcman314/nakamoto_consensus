abc6c2ccbd51c4b7859378bc13915c10632b9152
Marc Robison <marc.robison@etu.umontpellier.fr>
2025-02-13 09:44:02 +0100

    created transaction

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.serialization import load_pem_private_key, load_pem_public_key
 
-#class Transaction:
-#    pk_sender
-#    pk_receiver
-#    amount:int
-#    signature:bytes
+class Transaction:
+    pk_sender:ec.EllipticCurvePublicKey
+    pk_receiver:ec.EllipticCurvePublicKey
+    amount:int
+    signature:bytes
 
 #class Block:
     #header
@@ -17,11 +18,12 @@ from cryptography.hazmat.primitives.serialization import load_pem_private_key, l
 
 def main():
     message = b"Hello World!"
-    pr_key1, pub_key1 = import_key_pair("keys/ecc-key.pem", "keys/ecc-public.pem")
+    sk_key1, pub_key1 = import_key_pair("keys/ecc-key.pem", "keys/ecc-public.pem")
+    sk_key2, pub_key2 = import_key_pair("keys/ecc-key2.pem", "keys/ecc-public2.pem")
 
-    # Sign the message using the private key of the sender
+    new_transaction = createTransaction(sk_key1, pub_key1, pub_key2, 2)
+    printTransaction(new_transaction)
     
-
 
 
 
@@ -39,9 +41,8 @@ def import_key_pair(path_private_key:str, path_public_key:str):
     with open(path_public_key, "rb") as key_file:
         public_key = load_pem_public_key(key_file.read())
     if isinstance(public_key, ec.EllipticCurvePublicKey):
-        print("Public key imported successfully")
-        print(type(ec.EllipticCurvePublicKey))
-    else: print("Problem importing Public key")
+        print("Public key imported successfully\n")
+    else: print("Problem importing Public key\n")
 
     return private_key, public_key
 
@@ -49,32 +50,28 @@ def import_key_pair(path_private_key:str, path_public_key:str):
 
 
 
-    #def createTransaction(pk_sender:EllipticCurvePublicKey, pk_receiver:EllipticCurvePublicKey, amount:int):
-    #     pk_sender = pk_sender
-    #     pk_receiver = pk_receiver
-    #     amount:int = amount
-    # 
-    #     public_bytes = public_key.public_bytes(
-    #         encoding=serialization.Encoding.DER,  # Use DER for raw bytes
-    #         format=serialization.PublicFormat.SubjectPublicKeyInfo
-    #     )
-    #     print(public_bytes)  # Prints as b"..."
-    # 
-    # 
-    #     #message = 
-    # 
-    # 
-    # 
-    #     signature:int = pr_key1.sign(
-    #         message,
-    #         ec.ECDSA(hashes.SHA256()))
-    #     print(f"Signature: {signature.hex()}")
+def createTransaction(sk_sender:ec.EllipticCurvePrivateKey, pk_sender:ec.EllipticCurvePublicKey, pk_receiver:ec.EllipticCurvePublicKey, amount:int):
+
+    new_transaction = Transaction()
+
+    new_transaction.pk_sender = pk_sender
+    new_transaction.pk_receiver = pk_receiver
+    new_transaction.amount = amount
  
+    pk_sender_bytes = pk_sender.public_bytes(encoding=serialization.Encoding.DER, format=serialization.PublicFormat.SubjectPublicKeyInfo)
+    pk_receiver_bytes = pk_sender.public_bytes(encoding=serialization.Encoding.DER,format=serialization.PublicFormat.SubjectPublicKeyInfo)
+    message = pk_sender_bytes + pk_receiver_bytes + amount.to_bytes(4, 'big')
+    
+    new_transaction.signature = sk_sender.sign(
+       message,
+       ec.ECDSA(hashes.SHA256()))
+    return new_transaction
 
-
-
-
-
+def printTransaction(my_transaction:Transaction):
+    print(f"pk sender : {my_transaction.pk_sender}\n")
+    print(f"pk receiver : {my_transaction.pk_receiver}\n")
+    print(f"transaction amount : {my_transaction.amount}\n")
+    print(f"signature : {my_transaction.signature}\n")
 
 if __name__ == "__main__":
     main()
