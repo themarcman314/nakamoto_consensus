0313a5fd188d7fa33b8fdbb7d1229ee0056d9cc0
Marc Robison <marc@marcrobison.com>
2025-03-06 11:00:24 +0100

    hashed previous block

--- README.md
+++ README.md
@@ -5,8 +5,13 @@ git clone https://github.com/themarcman314/nakamoto_consensus.git
 ## Composition of a block
 header
 transactions
-nounce
+nounce : a number that combined with the header and transactions and hashed gives an output starting with a given number of 0's.
+The number of 0's corresponds to the mining difficulty
 
 header is the hash of the previous block
 
 genesis block has no header
+
+
+video that helped me understand nakamoto's concensus:
+https://www.youtube.com/watch?v=bBC-nXj3Ng4
--- main.py
+++ main.py
@@ -2,20 +2,22 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.serialization import load_pem_private_key, load_pem_public_key
+import numpy as np
+from colorama import Fore, Back, Style
 
 class Transaction:
     pk_sender:ec.EllipticCurvePublicKey
     pk_receiver:ec.EllipticCurvePublicKey
-    amount:int
+    amount:np.uint32
     signature:bytes
 
 class Block:
-    header
-    transactions[]:Transaction
-    footer
+    previous_block_hash:hashes.SHA256
+    difficulty:np.uint32
+    transactions:Transaction = []
+    nounce:np.uint32
 
 def main():
-    message = b"Hello World!"
     sk_key1, pub_key1 = import_key_pair("keys/ecc-key.pem", "keys/ecc-public.pem")
     sk_key2, pub_key2 = import_key_pair("keys/ecc-key2.pem", "keys/ecc-public2.pem")
 
@@ -24,6 +26,29 @@ def main():
     
     verifyTransaction(new_transaction, new_transaction.pk_sender)
 
+    random_data = "Hello World!"
+
+    random_hash = hashes.Hash(hashes.SHA256())
+    random_hash.update(random_data.encode("ascii"))
+
+    previous_block = random_hash.finalize()
+    
+    print(f"Previous block hash : \n{previous_block.hex()}")
+    
+    difficulty = np.uint32(3)
+
+    transactions:Transaction = []
+    transactions.append(new_transaction)
+
+    find_nounce(previous_block, difficulty, transactions)
+
+
+def find_nounce(previous_block_hash:hashes.SHA256, difficulty:np.uint32, transactions:Transaction = [])->Block:
+    nounce = 0
+    #block = previous_block_hash + difficulty.tobytes() + transactions + 
+
+
+    
 
 
 def import_key_pair(path_private_key:str, path_public_key:str):
@@ -34,14 +59,14 @@ def import_key_pair(path_private_key:str, path_public_key:str):
             password=None  # Use a password if the key is encrypted
         )
     if isinstance(private_key, ec.EllipticCurvePrivateKey):
-        print("✅ Private key imported successfully")
-    else: print("❌ Problem importing Private key")
+        print(Fore.GREEN + "✅ Private key imported successfully" + Style.RESET_ALL)
+    else: print(Fore.RED + "❌ Problem importing Private key" + Style.RESET_ALL)
 
     with open(path_public_key, "rb") as key_file:
         public_key = load_pem_public_key(key_file.read())
     if isinstance(public_key, ec.EllipticCurvePublicKey):
-        print("✅ Public key imported successfully\n")
-    else: print("❌ Problem importing Public key\n")
+        print(Fore.GREEN + "✅ Public key imported successfully\n" + Style.RESET_ALL)
+    else: print(Fore.RED + "❌ Problem importing Public key\n" + Style.RESET_ALL)
 
     return private_key, public_key
 
@@ -49,7 +74,7 @@ def import_key_pair(path_private_key:str, path_public_key:str):
 
 
 
-def createTransaction(sk_sender:ec.EllipticCurvePrivateKey, pk_sender:ec.EllipticCurvePublicKey, pk_receiver:ec.EllipticCurvePublicKey, amount:int):
+def createTransaction(sk_sender:ec.EllipticCurvePrivateKey, pk_sender:ec.EllipticCurvePublicKey, pk_receiver:ec.EllipticCurvePublicKey, amount:int)->Transaction:
 
     new_transaction = Transaction()
 
@@ -67,10 +92,19 @@ def createTransaction(sk_sender:ec.EllipticCurvePrivateKey, pk_sender:ec.Ellipti
     return new_transaction
 
 def printTransaction(my_transaction:Transaction):
-    print(f"pk sender : {my_transaction.pk_sender}\n")
-    print(f"pk receiver : {my_transaction.pk_receiver}\n")
-    print(f"transaction amount : {my_transaction.amount}\n")
-    print(f"signature : {my_transaction.signature}\n")
+    pk_sender_pem = my_transaction.pk_sender.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+    pk_receiver_pem = my_transaction.pk_receiver.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+    print("pk sender : \n" + Fore.CYAN + f"{pk_sender_pem}\n" + Style.RESET_ALL)
+    print("pk receiver : \n" + Fore.CYAN + f"{pk_receiver_pem}\n" + Style.RESET_ALL)
+    print(Fore.MAGENTA + f"transaction amount : {my_transaction.amount}\n" + Style.RESET_ALL)
+    print("signature : \n" + Fore.CYAN + f"{my_transaction.signature}\n" + Style.RESET_ALL)
 
 def verifyTransaction(transaction:Transaction, pk_sender:ec.EllipticCurvePublicKey):
     pk_sender:ec.EllipticCurvePublicKey
@@ -84,7 +118,7 @@ def verifyTransaction(transaction:Transaction, pk_sender:ec.EllipticCurvePublicK
 
     try:
         pk_sender.verify(transaction.signature, data, ec.ECDSA(hashes.SHA256()))
-        print("✅ Signature is valid!")
+        print(Fore.GREEN + "✅ Signature is valid!" + Style.RESET_ALL)
     except Exception as e:
         print(f"❌ Signature verification failed: {e}")
 
